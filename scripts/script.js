5148b5d5ff5088ce0960d6db080226122e187298
juanvillano <104040385+juanvillano@users.noreply.github.com>
2026-07-08 12:46:49 -0400

    add basic object and function structure

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+const myLibrary = [];
+
+function Book() {
+    // constructor
+    // id, author, title, number of pages, read
+}
+
+function addBookToLibrary() {
+    // take params, create a book then store it in the array
+}
+
+function displayBook() {
+    // loop through library and display books
+}
+
